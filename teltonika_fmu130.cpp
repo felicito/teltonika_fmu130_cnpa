3d1c416a4560d26a9c48700f1d073c9ab58638dd
Felícito Escobar <felicito.escobar@outlook.com>
2020-09-28 09:01:57 -0600

    version inicial

---
+++ teltonika_fmu130.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file teltonika_fmu130.cpp
+ * @author Felícito Manzano (felicito.manzano@detektor.com.sv)
+ * @brief 
+ * @version 0.1
+ * @date 2020-09-28
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
---
+++ teltonika_fmu130.hpp
@@ -0,0 +1,11 @@
+/**
+ * @file teltonika_fmu130.hpp
+ * @author Felícito Manzano (felicito.manzano@detektor.com.sv)
+ * @brief 
+ * @version 0.1
+ * @date 2020-09-28
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+

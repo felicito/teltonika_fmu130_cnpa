6caa4e4023696c16f3100b2935546e00a4fcfdc5
Felícito Escobar <felicito.escobar@outlook.com>
2020-09-28 11:48:18 -0600

    Actualizacion

--- teltonika_fmu130.cpp
+++ teltonika_fmu130.cpp
@@ -9,3 +9,36 @@
  * 
  */
 
+#include "BufferedSerial.h"
+
+// COMMANDS
+const int FMU130_COMMAND[]         = 65;
+const int SERVER_COMMAND[]         = 64;
+
+
+int read_fmu130_uart(BufferedSerial *puertoCOM, char m_buffer[1024]) {
+    int w = 0;
+    while (puertoCOM -> readable()) {
+        char incoming_char = puertoCOM -> getc();
+        m_buffer[w] = incoming_char;
+        w++;
+    }
+    m_buffer[w] = '\0';
+    return(w);
+}
+
+int identify_fmu130_payload(char m_buffer[1024]) {
+    int type_of_payload = -1;
+    char *pch;
+
+    pch = strchr(m_buffer, FMU130_COMMAND);
+    if (pch!=NULL) {
+        type_of_payload = 1;
+    } 
+
+    pch = strchr(m_buffer, SERVER_COMMAND);
+    if (pch!=NULL) {
+        type_of_payload = 2;
+    }
+    return(type_of_payload)
+}
--- teltonika_fmu130.hpp
+++ teltonika_fmu130.hpp
@@ -9,3 +9,13 @@
  * 
  */
 
+#ifndef __TELTONIKA_FMU130_HPP
+#define __TELTONIKA_FMU130_HPP
+
+    // COMMANDS
+    extern const int FMU130_COMMAND[];
+    extern const int SERVER_COMMAND[];  
+
+    int read_fmu130_uart(BufferedSerial *puertoCOM, char m_buffer[1024]);
+
+#endif // __TELTONIKA_FMU130_HPP
